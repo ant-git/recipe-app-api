20b51c1171f5c09511bf41a2962aab57c9b6134b
Anton <antonkhvdvgmail.com>
2019-08-24 00:54:00 +0300

    Updated admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,6 @@ class UserManager(BaseUserManager):
 
         return user
 
-
     def create_superuser(self, email, password):
         """creates and saves a new super user"""
         user = self.create_user(email, password)
@@ -24,6 +23,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instad of username"""
