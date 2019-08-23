7c9288ff822d5fd6a1ad274308cf2bb992b88692
Anton <antonkhvdvgmail.com>
2019-08-23 23:38:43 +0300

    Added create superuser method and test

--- app/core/models.py
+++ app/core/models.py
@@ -16,6 +16,15 @@ class UserManager(BaseUserManager):
         return user
 
 
+    def create_superuser(self, email, password):
+        """creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instad of username"""
     email = models.EmailField(max_length=255, unique=True)
--- app/core/tests/test__models.py
+++ app/core/tests/test__models.py
@@ -25,3 +25,12 @@ class ModelTests(TestCase):
         """Test creating user without email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """"Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@asdasad.com',
+            'test123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
