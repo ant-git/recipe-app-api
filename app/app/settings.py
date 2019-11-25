cb654f7bec903e6d24465dedc784a1083eca8eed
Anton <antonkhvdvgmail.com>
2019-11-25 17:22:53 +0300

    Fix typo

--- app/app/settings.py
+++ app/app/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environment.get('DB_HOST'),
-        'NAME': os.environment.get('DB_NAME'),
-        'USER': os.environment.get('DB_USER'),
-        'PASSWORD': os.environment.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
