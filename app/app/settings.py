ebb890b234993b31baa24d4215036c52abc8c404
Anton <antonkhvdvgmail.com>
2019-11-25 16:54:21 +0300

    Add postgres db

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql'
+        'HOST': os.environment.get('DB_HOST'),
+        'NAME': os.environment.get('DB_NAME'),
+        'USER': os.environment.get('DB_USER'),
+        'PASSWORD': os.environment.get('DB_PASS'),
     }
 }
 
