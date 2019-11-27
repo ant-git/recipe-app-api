8a394dc0c5010062d03c94e5e10e7929700ca6e8
Anton <antonkhvdv@gmail.com>
2019-11-27 14:54:10 +0300

    Add whitespaces

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -22,7 +22,7 @@ class PubicUserApiTests(TestCase):
     def test_create_valid_user_success(self):
         """Test creating user with a valid payload is successful"""
         payload = {
-            'email':'test@sometestemail.com',
+            'email': 'test@sometestemail.com',
             'password': 'testpass',
             'name': 'Test name',
         }
@@ -34,14 +34,14 @@ class PubicUserApiTests(TestCase):
 
     def test_user_exists(self):
         """Test creating user that already exists"""
-        payload = {'email': 'test@sometestemail.com', 'password':'testpass'}
+        payload = {'email': 'test@sometestemail.com', 'password': 'testpass'}
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short(self):
         """Test that the password should be more than 5 characters"""
-        payload = {"email":"test@sometestemail.com", 'password':'pw'}
+        payload = {"email": "test@sometestemail.com", 'password': 'pw'}
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
