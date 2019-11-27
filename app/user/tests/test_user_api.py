e651c511b80b970b10e1fbf3b99041e8c8ce692f
Anton <antonkhvdv@gmail.com>
2019-11-27 14:49:46 +0300

    Add create User API and fix tests

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -27,28 +27,24 @@ class PubicUserApiTests(TestCase):
             'name': 'Test name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
-
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(**res.data)
-        self.assertTrue(user.check_password(payload('password')))
+        self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
     def test_user_exists(self):
         """Test creating user that already exists"""
         payload = {'email': 'test@sometestemail.com', 'password':'testpass'}
         create_user(**payload)
-
         res = self.client.post(CREATE_USER_URL, payload)
-
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short(self):
         """Test that the password should be more than 5 characters"""
         payload = {"email":"test@sometestemail.com", 'password':'pw'}
         res = self.client.post(CREATE_USER_URL, payload)
-
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().object.filter(
+        user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
